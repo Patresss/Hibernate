f2b6cf137378e3e3ac1e20384e8f5d9a9a25de19
Patresss <piechaczek.patryk@gmail.com>
2022-07-26 21:37:59 +0200

    Add n+1 solver

--- src/main/java/com/globallogic/javaacademy/hibernate/repository/FarmerRepository.java
+++ src/main/java/com/globallogic/javaacademy/hibernate/repository/FarmerRepository.java
@@ -2,14 +2,20 @@ package com.globallogic.javaacademy.hibernate.repository;
 
 import com.globallogic.javaacademy.hibernate.entity.Farmer;
 import com.globallogic.javaacademy.hibernate.entity.Gender;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Set;
 
 public interface FarmerRepository extends JpaRepository<Farmer, Long> {
 
+
+    //@Query("select f From Farmer f join fetch f.contacts")
+//    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "contacts")
     Set<Farmer> findAllByGender(Gender gender);
+
 
     @Query("Select f from Farmer f where f.activeStatus = :activeStatus")
     Set<Farmer> findFarmerByActiveStatus(boolean activeStatus);
